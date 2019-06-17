f3217b34a38e6f69ef86462191a7612500df4459
adewunmi bamishigbin <32496428+adexbam@users.noreply.github.com>
2019-06-17 13:24:02 +0200

    add showModal function
    added the show modal function t

--- js/scripts.js
+++ js/scripts.js
@@ -36,10 +36,12 @@ var $pokemonRepository = (function () {     //IIFE starts here
         });
       };
 
+      //creat show modal function
       function showModal(item) {
-        var $modalTitle = $('.modal-title');   //creat element for name in modal content
-        var $modalId = $('#modal_id'); 
-        $modalTitle.text($modalId.text(item.name));
+        var $modalTitle = $('.modal-title');      //creat element for name in modal content
+        var $modalTitleTwo = $('<h6></h6>');      // creating h6 in modal content
+        $modalTitle.append($modalTitleTwo);       //appending modal content to webpage
+        $modalTitleTwo.text(item.name);
 
         var $modalBody = $('.modal-body');
         var $imageElement = $('<img>');         // creating img in modal content
